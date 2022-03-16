5ac21a2fa3090bd8378ea8f4d42af136c32b8f09
Chunyu <chunyu.j@icloud.com>
2022-03-17 02:20:54 +0800

    add: 带有获取 min 值的栈

---
+++ src/microsoft/MinStack.kt
@@ -0,0 +1,50 @@
+package microsoft
+
+import java.util.Stack
+
+
+/**
+ * 包含 min 函数的湛
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.min()
+ */
+class MinStack() {
+
+    /** initialize your data structure here. */
+    var stack = Stack<Int>()
+    var minStack = Stack<Int>()
+    var min = Int.MAX_VALUE
+
+    fun push(x: Int) {
+        stack.add(x)
+        min = if (min > x) {
+            x
+        } else {
+            min
+        }
+        minStack.add(min)
+    }
+
+    fun pop() {
+        stack.pop()
+        minStack.pop()
+        // 注意每次 pop ， min 最小值会发生变化。
+        min = if (minStack.isEmpty()) {
+            Int.MAX_VALUE
+        } else {
+            minStack.peek()
+        }
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun min(): Int {
+        return minStack.peek()
+    }
+}
