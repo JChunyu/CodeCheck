f8d13fb648dc4f3fb5906dc7b5e4603542d3dc73
Chunyu <chunyu.j@icloud.com>
2022-03-19 14:34:46 +0800

    add: add string day 3

--- src/common/struct/ListNode.kt
+++ src/common/struct/ListNode.kt
@@ -2,4 +2,5 @@ package common.struct
 
 class ListNode(var value: Int) {
     var next: ListNode? = null
+    var random: ListNode? = null
 }
---
+++ src/microsoft/list/ReverseList.kt
@@ -0,0 +1,36 @@
+package microsoft.list
+
+import common.struct.ListNode
+
+class ReverseList {
+    fun reverseList(head: ListNode?): ListNode? {
+        var node = head
+        var reverse: ListNode? = null
+        while (node != null) {
+            val temp = node.next
+            node.next = reverse
+            reverse = node
+            node = temp
+        }
+        return reverse
+    }
+}
+
+fun main() {
+    val node1 = ListNode(1)
+    val node2 = ListNode(2)
+    val node3 = ListNode(3)
+    val node4 = ListNode(4)
+    val node5 = ListNode(5)
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+
+    var a = ReverseList().reverseList(node1)
+
+    while (a != null) {
+        println(a.value)
+        a = a.next
+    }
+}
