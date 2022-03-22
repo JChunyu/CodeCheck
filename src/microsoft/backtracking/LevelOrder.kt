401cfe7c5eea46e68f7d8fd29560d49c6677eb83
Chunyu <chunyujiang@sf-express.com>
2022-03-23 01:21:15 +0800

    add: levelOrder

--- src/microsoft/backtracking/LevelOrder.kt
+++ src/microsoft/backtracking/LevelOrder.kt
@@ -64,37 +64,33 @@ class LevelOrder {
     * 第二层按照从右到左的顺序打印，
     * 第三行再按照从左到右的顺序打印，其他行以此类推。
     * */
-    fun levelOrder(root: TreeNode?): List<List<Int>> {
+    // 其实是 levelOrder3 的升级
+    fun levelOrder3(root: TreeNode?): List<List<Int>> {
         val result = ArrayList<List<Int>>()
-        if (root == null) {
-            return result
-        }
-        val queue = LinkedList<ArrayList<TreeNode>>() // 存每一层数据
-        queue.add(arrayListOf(root))
-        var seq = true
-        while (queue.isNotEmpty()) {
-            val curLayers = queue.poll()
-            val nextLayers = arrayListOf<TreeNode>()
-            val curRes = arrayListOf<Int>()
+        if (root == null) return result
+        val list = LinkedList<TreeNode>()
+        var seq = false
+        list.add(root)
+        while (list.isNotEmpty()) {
+            val size = list.size
+            val temList = ArrayList<Int>()
             seq = !seq
+            for (i in 0 until size) {
+                val current = list.poll()
+                temList.add(current.`val`)
+                current.left?.let { left ->
+                    list.add(left)
+                }
+                current.right?.let { right ->
+                    list.add(right)
+                }
+            }
             if (seq) {
-                val temp = curLayers.reversed()
-                curLayers.clear()
-                curLayers.addAll(temp)
+                val a = temList.reversed()
+                temList.clear()
+                temList.addAll(a)
             }
-            curLayers.forEach { it ->
-                curRes.add(it.`val`)
-                it.left?.let { left ->
-                    nextLayers.add(left)
-                }
-                it.right?.let { right ->
-                    nextLayers.add(right)
-                }
-            }
-            if (curLayers.isNotEmpty()) {
-                queue.add(nextLayers)
-                result.add(curRes)
-            }
+            result.add(temList)
         }
         return result
     }
@@ -102,15 +98,15 @@ class LevelOrder {
 
 fun main() {
     val levelOrder = LevelOrder()
-    val treeNode1 = TreeNode(3)
-    val treeNode2 = TreeNode(9)
-    val treeNode3 = TreeNode(20)
-    val treeNode4 = TreeNode(15)
-    val treeNode5 = TreeNode(7)
+    val treeNode1 = TreeNode(1)
+    val treeNode2 = TreeNode(2)
+    val treeNode3 = TreeNode(3)
+    val treeNode4 = TreeNode(4)
+    val treeNode5 = TreeNode(5)
 
     treeNode1.left = treeNode2
     treeNode1.right = treeNode3
-    treeNode3.left = treeNode4
+    treeNode2.left = treeNode4
     treeNode3.right = treeNode5
 
     levelOrder.levelOrder3(treeNode1)
