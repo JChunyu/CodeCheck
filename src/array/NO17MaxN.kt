18116440e7f935cf4d892c879938b1a9cf8a0b81
chunyu <chunyu.j@icloud.com>
2022-04-22 01:04:23 +0800

    add: files

---
+++ src/array/NO17MaxN.kt
@@ -0,0 +1,21 @@
+package array
+
+class NO17MaxN {
+    fun printNumbers(n: Int): IntArray {
+        val max = getMax(n)
+        val array = IntArray(max - 1)
+        for (i in 1 until max) {
+            array[i - 1] = i
+        }
+        return array
+    }
+
+    fun getMax(x: Int): Int {
+        if (x == 1) return 1
+        return getMax(x - 1) * 10
+    }
+}
+
+fun main() {
+    val maxN = NO17MaxN().printNumbers(3)
+}
